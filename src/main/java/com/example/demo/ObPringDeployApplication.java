86fb37ec573ea774e3c68651b1dac8507954311f
DMN0is3 <danielmorenomula@gmail.com>
2022-04-06 19:46:09 +0200

    Spring Heroku App

---
+++ src/main/java/com/example/demo/ObPringDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObPringDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObPringDeployApplication.class, args);
+	}
+
+}
